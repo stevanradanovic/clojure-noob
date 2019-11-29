a773f2107562d2600077a1f593ae5517c68a8981
Stevan Radanovic <stevan.radanovic@gmail.com>
2019-11-29 17:18:23 +0100

    smackity smack init

---
+++ 03-do-things/hobbit-smacker/src/hobbit_smacker/core.clj
@@ -0,0 +1,7 @@
+(ns hobbit-smacker.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... but I can smack some hobbits around!"
+  [& args]
+  (println "Smackity smack!"))
---
+++ 03-do-things/hobbit-smacker/test/hobbit_smacker/core_test.clj
@@ -0,0 +1,7 @@
+(ns hobbit-smacker.core-test
+  (:require [clojure.test :refer :all]
+            [hobbit-smacker.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
