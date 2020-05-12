1787d9b59a00f82a7fe746e1efafe2d4a62e4ad6
Stevan Radanovic <stevan.radanovic@gmail.com>
2020-05-13 01:47:18 +0200

    exercise 03

--- 03-do-things/exercises/src/exercises/core.clj
+++ 03-do-things/exercises/src/exercises/core.clj
@@ -1,12 +1,25 @@
 (ns exercises.core
   (:gen-class))
 
+;;; Exercise 02
+
 (defn exer02
   "Function that takes a number and adds 100 to it"
   [num]
   (+ 100 num))
 
+;;; Exercise 03
+
+(defn dec-maker
+  "Makes decrementer function"
+  [dec-by]
+  (fn [num] (- num dec-by)))
+
+(def dec9 (dec-maker 9))
+
+
 (defn -main
   "I don't do a whole lot... But I can call few functions."
   []
-  (println "I'm gonna add 100 to 42:" (exer02 42)))
+  (println "I'm gonna add 100 to 42:" (exer02 42))
+  (println "I'm gonna subtract 9 from 42:" (dec9 42)))
