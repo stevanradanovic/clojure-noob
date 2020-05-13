e9bb839819be0a34ec489c7b93c2878e9c0e31be
Stevan Radanovic <stevan.radanovic@gmail.com>
2020-05-13 13:14:42 +0200

    exercise 04

--- 03-do-things/exercises/src/exercises/core.clj
+++ 03-do-things/exercises/src/exercises/core.clj
@@ -17,9 +17,17 @@
 
 (def dec9 (dec-maker 9))
 
+;;; Exercise 04
+
+(defn mapset
+  "Map that returns a set"
+  [func coll]
+  (set (map func coll)))
+
 
 (defn -main
   "I don't do a whole lot... But I can call few functions."
   []
   (println "I'm gonna add 100 to 42:" (exer02 42))
-  (println "I'm gonna subtract 9 from 42:" (dec9 42)))
+  (println "I'm gonna subtract 9 from 42:" (dec9 42))
+  (println "I'm gonna inc and set on [42, 42, 1]:" (mapset inc [42, 42, 1])))
