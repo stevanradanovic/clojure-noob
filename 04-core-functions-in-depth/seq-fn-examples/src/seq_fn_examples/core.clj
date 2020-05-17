849abf116de99bddd4cfbcf878a9c078505a1d3f
Stevan Radanovic <stevan.radanovic@htecgroup.com>
2020-05-17 16:59:46 +0200

    my-map using reduce

--- 04-core-functions-in-depth/seq-fn-examples/src/seq_fn_examples/core.clj
+++ 04-core-functions-in-depth/seq-fn-examples/src/seq_fn_examples/core.clj
@@ -2,10 +2,17 @@
   (:gen-class))
 
 ;; Implement map, filter and some using reduce
-
+(defn my-map
+  "Map implemented using reduce"
+  [func coll]
+  (reduce (fn [new-coll first]
+            (into new-coll (func first)))
+          #()
+          coll))
 
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (println "My-map increment [1 2 3]:" (my-map inc [1 2 3])))
