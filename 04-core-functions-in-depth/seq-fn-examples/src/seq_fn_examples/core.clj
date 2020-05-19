b245663ebbf079cb12d19f56aa5f5a0a16595803
Stevan Radanovic <stevan.radanovic@gmail.com>
2020-05-19 21:31:19 +0200

    interesting map example

--- 04-core-functions-in-depth/seq-fn-examples/src/seq_fn_examples/core.clj
+++ 04-core-functions-in-depth/seq-fn-examples/src/seq_fn_examples/core.clj
@@ -1,6 +1,13 @@
 (ns seq-fn-examples.core
   (:gen-class))
 
+;; Map list of functions onto collection
+(def sum #(reduce + %))
+(def avg #(/ (sum %) (count %)))
+(defn stats
+  [numbers]
+  (map #(% numbers) [sum count avg]))
+
 ;; Implement map, filter and some using reduce
 (defn my-map
   "Map implemented using reduce"
@@ -14,4 +21,5 @@
 (defn -main
   "I don't do a whole lot ... yet."
   []
+  (println "Sum, count and avg for [42 0 7]:" (stats [42 0 7]))
   (println "My-map increment [1 2 3]:" (my-map inc [1 2 3])))
