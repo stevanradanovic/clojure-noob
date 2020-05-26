b49da3622a9c91a4c0271f3b2c27a7b9646948fa
stevanradanovic <stevan.radanovic@gmail.com>
2020-05-26 18:40:31 +0200

    fwpd checkpoint

--- 04-core-functions-in-depth/fwpd/src/fwpd/core.clj
+++ 04-core-functions-in-depth/fwpd/src/fwpd/core.clj
@@ -25,9 +25,17 @@
   "Return a seq of maps like {:name \"Edward Cullen\" :glitter-index 10}"
   [rows]
   (map (fn [unmapped-row]
-         (reduce ()))))
+         (reduce (fn [row-map [vamp-key value]]
+                   (assoc row-map vamp-key (convert vamp-key value)))
+                 {}
+                 (map vector vamp-keys unmapped-row)))
+       rows))
+
+(defn glitter-filter
+  [minimum-glitter records]
+  (filter #(>= (:glitter-index %) minimum-glitter) records))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  "I find vampires!"
+  []
+  (println (glitter-filter 3 (mapify (parse (slurp filename))))))
