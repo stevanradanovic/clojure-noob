0db19be61dc64ac9cde1aca37671e709e43ba76e
stevanradanovic <stevan.radanovic@gmail.com>
2020-05-26 17:38:56 +0200

    fwpd tmp

--- 04-core-functions-in-depth/fwpd/src/fwpd/core.clj
+++ 04-core-functions-in-depth/fwpd/src/fwpd/core.clj
@@ -2,6 +2,30 @@
 
 (def filename "suspects.csv")
 
+(def vamp-keys [:name :glitter-index])
+
+(defn str->int
+  [str]
+  (Integer. str))
+
+(def conversions
+  {:name identity :glitter-index str->int})
+
+(defn convert
+  [vamp-key value]
+  ((get conversions vamp-key) value))
+
+(defn parse
+  "Parse data from CSV into seq"
+  [data]
+  (map #(clojure.string/split % #",")
+       (clojure.string/split data #"\n")))
+
+(defn mapify
+  "Return a seq of maps like {:name \"Edward Cullen\" :glitter-index 10}"
+  [rows]
+  (map (fn [unmapped-row]
+         (reduce ()))))
 
 (defn -main
   "I don't do a whole lot ... yet."
