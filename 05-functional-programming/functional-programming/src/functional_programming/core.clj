23ec5c85086d1a45143d9e4e63a3e97328c7e540
stevanradanovic <stevan.radanovic@gmail.com>
2020-06-03 00:28:14 +0200

    fibonacci using memoization

--- 05-functional-programming/functional-programming/src/functional_programming/core.clj
+++ 05-functional-programming/functional-programming/src/functional_programming/core.clj
@@ -19,6 +19,19 @@
       init-value
       (recur (rest coll) (+ (first coll) init-value)))))
 
+(defn my-comp
+  "Implementation of the comp function"
+  []
+  ())
+
+(def fib-mem
+  "Fibonacci with memoization"
+  (memoize (fn [num]
+             (condp = num
+               0 1
+               1 1
+               (+ (fib-mem (dec num)) (fib-mem (- num 2)))))))
+
 (defn -main
   "I don't do a whole lot... exept showcase functional programming."
   []
