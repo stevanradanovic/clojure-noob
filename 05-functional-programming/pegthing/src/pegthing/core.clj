87dcb1b87863e9783ddebe934ea22791b9044c4a
stevanradanovic <stevan.radanovic@gmail.com>
2020-06-09 21:06:53 +0200

    triangular numbers

--- 05-functional-programming/pegthing/src/pegthing/core.clj
+++ 05-functional-programming/pegthing/src/pegthing/core.clj
@@ -4,6 +4,15 @@
 
 (declare successful-move prompt-move game-over query-rows)
 
+(defn tri*
+  "Generates lazy sequence of triangular numbers"
+  ([] (tri* 0 1))
+  ([sum n]
+    (let [new-sum (+ sum n)]
+      (cons new-sum (lazy-seq (tri* new-sum (inc n)))))))
+
+(def tri (tri*))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
