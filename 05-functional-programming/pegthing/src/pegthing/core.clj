45601ed8f8eeff5312533115043a8e5b7de82447
stevanradanovic <stevan.radanovic@gmail.com>
2020-06-13 20:45:14 +0200

    rebasing the changes

--- 05-functional-programming/pegthing/src/pegthing/core.clj
+++ 05-functional-programming/pegthing/src/pegthing/core.clj
@@ -1,5 +1,5 @@
 (ns pegthing.core
-  (require [clojure.set :as set])
+  (:require [clojure.set :as set])
   (:gen-class))
 
 (declare successful-move prompt-move game-over query-rows)
@@ -12,6 +12,12 @@
       (cons new-sum (lazy-seq (tri* new-sum (inc n)))))))
 
 (def tri (tri*))
+
+(defn blablabla [num] (println num))
+
+(defn test [] (println "test"))
+
+(defn blabla [num] (println "test" num))
 
 (defn -main
   "I don't do a whole lot ... yet."
