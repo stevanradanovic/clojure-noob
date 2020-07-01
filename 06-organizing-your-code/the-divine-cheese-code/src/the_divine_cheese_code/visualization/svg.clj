9cfb7eb5392d907a517931c1da3c994fca509155
stevanradanovic <stevan.radanovic@gmail.com>
2020-07-01 21:40:24 +0200

    organizing - comparator

--- 06-organizing-your-code/the-divine-cheese-code/src/the_divine_cheese_code/visualization/svg.clj
+++ 06-organizing-your-code/the-divine-cheese-code/src/the_divine_cheese_code/visualization/svg.clj
@@ -1,4 +1,6 @@
-(ns the-divine-cheese-code.visualization.svg)
+(ns the-divine-cheese-code.visualization.svg
+  (:require [clojure.string :as string])
+  (:refer-clojure :exclude [min max]))
 
 (defn latlng->point
   "Convert lat/lng map to comma-separated string"
@@ -7,4 +9,15 @@
 
 (defn points
   [locations]
-  (clojure.string/join " " (map latlng->point locations)))
+  (string/join " " (map latlng->point locations)))
+
+(defn comparator-over-maps
+  [comparison-fn ks]
+  (fn [maps]
+    (zipmap ks
+            (map (fn [k] (apply comparison-fn (map k maps)))
+                 ks))))
+
+(def min (comparator-over-maps clojure.core/min [:lat :lng]))
+
+(def max (comparator-over-maps clojure.core/max [:lat :lng]))
