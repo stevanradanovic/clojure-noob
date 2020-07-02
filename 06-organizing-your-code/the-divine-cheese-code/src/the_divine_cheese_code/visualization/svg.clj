21411a4232cdd89c0f3dc80d371cd5564fe51ef3
stevanradanovic <stevan.radanovic@gmail.com>
2020-07-02 16:35:23 +0200

    organizing - scaling locations

--- 06-organizing-your-code/the-divine-cheese-code/src/the_divine_cheese_code/visualization/svg.clj
+++ 06-organizing-your-code/the-divine-cheese-code/src/the_divine_cheese_code/visualization/svg.clj
@@ -12,12 +12,24 @@
   (string/join " " (map latlng->point locations)))
 
 (defn comparator-over-maps
-  [comparison-fn ks]
+  [comparison-fn keys]
   (fn [maps]
-    (zipmap ks
-            (map (fn [k] (apply comparison-fn (map k maps)))
-                 ks))))
+    (zipmap keys
+            (map (fn [key] (apply comparison-fn (map key maps)))
+                 keys))))
 
 (def min (comparator-over-maps clojure.core/min [:lat :lng]))
 
 (def max (comparator-over-maps clojure.core/max [:lat :lng]))
+
+(defn translate-to-00
+  [locations]
+  (let [mincoords (min locations)]
+    (map #(merge-with - % mincoords) locations)))
+
+(defn scale
+  [width height locations]
+  (let [maxcoords (max locations)
+        ratio {:lat (/ height (:lat maxcoords))
+               :lng (/ width (:lng maxcoords))}]
+    (map #(merge-with * % ratio) locations)))
