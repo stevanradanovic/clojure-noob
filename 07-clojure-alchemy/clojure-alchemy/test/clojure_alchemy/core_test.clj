a69ba78ae29bdddbad8ced85fbe2286bd58338ae
stevanradanovic <stevan.radanovic@gmail.com>
2020-07-03 22:43:37 +0200

    clojure-alchemy - init

---
+++ 07-clojure-alchemy/clojure-alchemy/test/clojure_alchemy/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-alchemy.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-alchemy.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
