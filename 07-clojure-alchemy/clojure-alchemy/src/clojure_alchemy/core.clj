14626fa9d8829eb51b8074c8b916bf3a6cd91776
stevanradanovic <stevan.radanovic@gmail.com>
2020-07-05 17:17:17 +0200

    clojure-alchemy - basic-infix

--- 07-clojure-alchemy/clojure-alchemy/src/clojure_alchemy/core.clj
+++ 07-clojure-alchemy/clojure-alchemy/src/clojure_alchemy/core.clj
@@ -1,7 +1,13 @@
 (ns clojure-alchemy.core
   (:gen-class))
 
+(defmacro basic-infix
+  [infixed]
+  (list (second infixed)
+        (first infixed)
+        (last infixed)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  "I don't do a whole lot... but I can infix."
+  []
+  (println "1 + 2 =" (basic-infix (1 + 2))))
