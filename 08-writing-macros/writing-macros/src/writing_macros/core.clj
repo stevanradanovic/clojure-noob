836a24e44bca8b43f686f7adb8c748121d424998
stevanradanovic <stevan.radanovic@gmail.com>
2020-07-09 19:29:30 +0200

    writing-macros - basic-infix

--- 08-writing-macros/writing-macros/src/writing_macros/core.clj
+++ 08-writing-macros/writing-macros/src/writing_macros/core.clj
@@ -1,6 +1,16 @@
 (ns writing-macros.core
   (:gen-class))
 
+(defmacro basic-infix
+  "Use this macro when you pine for the notation
+   of your childhood"
+  [infixed]
+  (list (second infixed)
+        (first infixed)
+        (last infixed)))
+
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
