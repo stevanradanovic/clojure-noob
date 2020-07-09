4c0b6b853459c6f12c2d26aaa100de2a3bdac2f4
stevanradanovic <stevan.radanovic@gmail.com>
2020-07-09 22:19:58 +0200

    writing-macros - code-critic

--- 08-writing-macros/writing-macros/src/writing_macros/core.clj
+++ 08-writing-macros/writing-macros/src/writing_macros/core.clj
@@ -13,8 +13,17 @@
   [[operand-1 op operand-2]]
   (list op operand-1 operand-2))
 
+(defmacro code-critic
+  "Phrases are courtesy Hermes Conrad from Futurama"
+  [bad good]
+  `(do (println "Great squid of Madrid, this is bad code:"
+                (quote ~bad))
+       (println "Sweet gorilla of Manila, this is good code:"
+                (quote ~good))))
+
 (defn -main
   "I don't do a whole lot... but a few macros."
   []
   (println "Let's try macroing (1 + 2):" (basic-infix (1 + 2)))
-  (println "Let's try another (1 + 2):" (basic-infix-2 (1 + 2))))
+  (println "Let's try another (1 + 2):" (basic-infix-2 (1 + 2)))
+  (println (code-critic (1 + 2) (+ 1 2))))
