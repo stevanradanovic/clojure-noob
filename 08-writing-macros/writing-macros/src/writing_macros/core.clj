399776d4d2ed80479295dcc55def6084380047ce
stevanradanovic <stevan.radanovic@gmail.com>
2020-07-09 19:34:38 +0200

    writing-macros - basic-infix-2

--- 08-writing-macros/writing-macros/src/writing_macros/core.clj
+++ 08-writing-macros/writing-macros/src/writing_macros/core.clj
@@ -9,9 +9,12 @@
         (first infixed)
         (last infixed)))
 
-
+(defmacro basic-infix-2
+  [[operand-1 op operand-2]]
+  (list op operand-1 operand-2))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  "I don't do a whole lot... but a few macros."
+  []
+  (println "Let's try macroing (1 + 2):" (basic-infix (1 + 2)))
+  (println "Let's try another (1 + 2):" (basic-infix-2 (1 + 2))))
