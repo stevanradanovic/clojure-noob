7d0b5497e157639f0657ce96bc10c2e9f0bfc450
stevanradanovic <stevan.radanovic@gmail.com>
2020-07-13 23:16:17 +0200

    writing-macros - variable capture

--- 08-writing-macros/writing-macros/src/writing_macros/core.clj
+++ 08-writing-macros/writing-macros/src/writing_macros/core.clj
@@ -21,9 +21,28 @@
        (println "Sweet gorilla of Manila, this is good code:"
                 (quote ~good))))
 
+(def message "Good job!")
+
+(defmacro with-mischief
+  [& stuff-to-do]
+  (concat (list 'let ['message "Oh, big deal!"])
+          stuff-to-do))
+
+(defmacro without-mischief
+  [& stuff-to-do]
+  (let [macro-message (gensym 'message)]
+    `(let [~macro-message "Oh, big deal!"]
+       ~@stuff-to-do
+       (println "I still need to say: " ~macro-message))))
+
 (defn -main
   "I don't do a whole lot... but a few macros."
   []
   (println "Let's try macroing (1 + 2):" (basic-infix (1 + 2)))
   (println "Let's try another (1 + 2):" (basic-infix-2 (1 + 2)))
-  (println (code-critic (1 + 2) (+ 1 2))))
+  (println (code-critic (1 + 2) (+ 1 2)))
+  ;; This would print "Oh, big deal!" instead of "Good job!"
+  ;; because of variable capture
+  ;; (with-mischief (println "How I feel about your success: " message))
+  (without-mischief
+    (println "How I feel about your success: " message)))
